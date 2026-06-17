1045afcf5699aface409338b14a9a6fe847ff08b
Hamza Rana <hamzasr6122@gmail.com>
2026-06-17 13:11:18 -0400

    Updated

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 
-import express from 'express'
-const app = express()
+// import express from 'express'
+// const app = express()
 
-console.log("hello world");
+// console.log("hello world");
 
-app.get("https://pokeapi.co/api/v2/pokemon/ditto" , (req, res) => {
-    res.send();
+// app.get("https://pokeapi.co/api/v2/pokemon/ditto" , (req, res) => {
+//     res.send();
 
-})
-
-
-
- fetch('https://pokeapi.co/api/v2/pokemon/ditto')
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
+// })
 
 
-app.listen(3000, () => {
-    console.log("Server Online")
-});
+
+
+ const res = await fetch('https://pokeapi.co/api/v2/pokemon/ditto') 
+    const data = await res.json();
+    console.log("Data:",data);
+
+
+// Server Setup
+// app.listen(3000, () => {
+//     console.log("Server Online")
+// });
